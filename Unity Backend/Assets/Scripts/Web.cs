d2a35d8a3caac47db6b15316bd57a5c3b8cd7bfc
Rasoul Sharifi <rasoul.sharifi1988@gmail.com>
2023-07-15 00:51:26 +0330

    init project

---
+++ Unity Backend/Assets/Scripts/Web.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Web : MonoBehaviour
+{
+
+
+
+
+    void Start()
+    {
+        StartCoroutine(GetRequest("http://localhost/UnityBackendTutorial/GetData.php"));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+                Debug.Log(www.error);
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                byte[] results = www.downloadHandler.data;
+            }
+
+
+            //string[] pages = uri.Split('/');
+            //int page = pages.Length - 1;
+
+            //switch (www.result)
+            //{
+            //    case UnityWebRequest.Result.ConnectionError:
+            //    case UnityWebRequest.Result.DataProcessingError:
+            //        Debug.LogError(pages[page] + ": Error: " + www.error);
+            //        break;
+            //    case UnityWebRequest.Result.ProtocolError:
+            //        Debug.LogError(pages[page] + ": HTTP Error: " + www.error);
+            //        break;
+            //    case UnityWebRequest.Result.Success:
+            //        Debug.Log(pages[page] + ":\nReceived: " + www.downloadHandler.text);
+            //        break;
+            //}
+        }
+    }
+
+
+
+}
